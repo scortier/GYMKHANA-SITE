c4e5f826c929cdfd711c72d54a903928555c3cc8
milibar30 <milind.barman30@gmail.com>
2020-09-28 13:27:59 +0530

    Front end changes completed,more info in comments

---
+++ app.js
@@ -0,0 +1,139 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+const { render } = require("ejs");
+
+
+app.use(express.static("public"));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+
+const connectDB = async ()=>{
+    await mongoose.connect('mongodb+srv://aditya2712:aditya2712@cluster0.budh1.mongodb.net/events?retryWrites=true&w=majority',{ 
+        useUnifiedTopology: true,
+        useNewUrlParser: true 
+    });
+    console.log("database connected!");  
+}
+
+connectDB();
+
+var eventSchema = new mongoose.Schema({
+    heading: String,
+    details: String,
+    postedBy: String,
+    category: String,
+    links: String,
+    deadline: { type: Date, default: Date.now },
+    postedOn: { type: Date, default: Date.now }
+});
+
+//Create Mongoose Model
+var Event = mongoose.model("Event", eventSchema);
+
+//adminForm 
+app.get("/adminform",function(req,res){
+    res.render("adminForm.ejs");
+})
+
+app.post("/adminform",function(req,res){
+    var newEvent=new Event(req.body);
+    newEvent.save();
+    // console.log(req.body);
+    res.send("Event Created!!") ;
+})
+
+//Default Routing
+app.get("/", function(req, res) {
+    res.redirect("/index");
+});
+
+//Home page
+app.get("/index", function(req, res) {
+    res.render("index.ejs");
+});
+
+//Fest Page
+app.get("/fest", function(req, res) {
+    res.render("FEST.ejs");
+});
+
+//Activities Page
+app.get("/activities", function(req, res) {
+    res.render("activities.ejs");
+});
+
+//Campus Life Page
+app.get("/campus", function(req, res) {
+    res.render("Campus_Life.ejs");
+});
+
+//Members Page
+app.get("/members", function(req, res) {
+    res.render("Current_Members(2019).ejs");
+});
+
+// Events Page
+app.get("/events", function(req, res) {
+    Event.find({},null, {sort: {postedOn: -1} } , function(err, allEvents) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("events2.ejs", {
+                events: allEvents
+            });
+        }
+    })
+});
+
+//Oldmembers Page
+app.get("/oldmembers", function(req, res) {
+    res.render("oldmembers.ejs");
+});
+
+//Oldmembers2 Page
+app.get("/oldmembers2", function(req, res) {
+    res.render("oldmembers2.ejs");
+});
+
+//Oldmembers3 Page
+app.get("/oldmembers3", function(req, res) {
+    res.render("oldmembers3.ejs");
+});
+
+//Societies Page
+app.get("/societies", function(req, res) {
+    res.render("societies.ejs");
+});
+
+//Event Details page
+let documentId;
+app.get("/eventdetail/:id",function(req,res){
+    documentId=req.params.id;
+    res.redirect("/eventdetail")
+})
+app.get("/eventdetail",function(req, res){
+    Event.findOne( {_id:documentId} ,(err,event) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("eventDetail.ejs", {
+                event: event
+            })
+        }
+    })
+})
+
+//########################################################
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("SERVER STARTED!!");
+});
